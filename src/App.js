a3d0fa37c482a87f1c4caf2d5dabd5576525bde0
Shreyas <paridayash520@gmail.com>
2024-05-31 21:47:17 +0530

    search from list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import Result from "./Components/Result";
+import Body from "./Components/Body";
 
 function App() {
+  const NameList = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const [totalMatch, setTotalMatch] = useState(0);
+  const [result, setResult] = useState([]);
+  const check = (name) => {
+    let newResult = [];
+    let newTotalMatch = 0;
+    let count = 0;
+    NameList.map((n) => {
+      if (n.toLowerCase().includes(name.toLowerCase())) {
+        newTotalMatch++;
+        newResult.push(n);
+      }
+    });
+    setResult(newResult);
+    setTotalMatch(newTotalMatch);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="container">
+        <Body check={check} />
+      </main>
+      <Result result={result} totalMatch={totalMatch} />
+    </>
   );
 }
 
